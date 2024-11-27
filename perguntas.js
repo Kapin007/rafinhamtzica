0b91bca9eadea0ef94d73aa722e8785fbe5d51d2
Kapin007 <kawasaki.jogos@gmail.com>
2024-11-26 21:30:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que faz o Python ser uma linguagem popular?',
+    'Python é uma linguagem fácil de aprender, com uma sintaxe clara e uma vasta biblioteca de recursos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica a Torre Eiffel?',
+    'A Torre Eiffel está localizada em Paris, na França.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre função e método?',
+    'Funções são blocos de código independentes, enquanto métodos são funções associadas a objetos em programação orientada a objetos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a tradução de "How are you?"?',
+    'A tradução de "How are you?" é "Como você está?"'
+)
